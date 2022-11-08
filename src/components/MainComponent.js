e1fd7aef05a39e02654527f4f7d090730aa2283d
Jajhoey <john.hoey15@my.stjohns.edu>
2022-11-08 15:23:59 -0500

    Assignment 2, About Us section and Leaders List

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,6 +9,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
+import About from './AboutComponent';
 import {Switch, Route, Redirect} from 'react-router-dom';
 
 
@@ -41,6 +42,11 @@ class Main extends Component {
       );
     }
 
+    const AboutUs = () => {
+      return (
+        <About leaders = {this.state.leaders}/>
+      );
+    }
     return (
       <div>
         <Header/>
@@ -50,6 +56,7 @@ class Main extends Component {
           <Route path ='/menu/:dishId' component={DishWithID}/>
           <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes}/>} />
           <Route exact path='/contactus' component={Contact}/>
+          <Route exact path='/aboutus' component={AboutUs}/>
           <Redirect to='/home'/>
         </Switch>
         <Footer/>
